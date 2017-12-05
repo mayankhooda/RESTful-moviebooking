accd539f0f678194a9339de81f5341a2a02645e7
Mayank Hooda <mayankhooda@yahoo.com>
2017-12-05 08:55:34 +0530

    add index route and server

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
     request = require("request"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    app = express();
 
+app.get("/", function(req, res){
+    res.send("HOME PAGE!");
+});
+
+app.listen(3000, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("SERVER IS LISTENING!!!");
+    }
+});
